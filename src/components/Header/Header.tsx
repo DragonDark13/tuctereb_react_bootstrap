3f2a26a6e54af324a7b93be3fd578349f8d9019f
Aleksandr <aleksdark13@gmail.com>
2024-09-11 19:23:02 +0300

    header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,78 +1,81 @@
-import React, { useState, useEffect, useRef } from "react";
+import {useState, useEffect, useRef} from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import '../../App.scss';
 import Sticky from 'react-sticky-el';
-import { Link, Button } from 'react-scroll';
+import {Link, Button} from 'react-scroll';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 
 interface IHeader {
-  setHeaderHeight: (arg0: number) => void;
+    setHeaderHeight: (arg0: number) => void;
 }
 
-const Header = ({ setHeaderHeight }: IHeader) => {
-  const headerRef = useRef<HTMLDivElement>(null);
-  const [isMenuOpen, setIsMenuOpen] = useState(false); // Стан для відкритого меню
+const Header = ({setHeaderHeight}: IHeader) => {
+    const headerRef = useRef<HTMLDivElement>(null);
+    const [isMenuOpen, setIsMenuOpen] = useState(false); // Стан для відкритого меню
 
-  useEffect(() => {
-    if (headerRef.current && headerRef.current.offsetHeight) {
-      console.log('Height of header:', headerRef.current.offsetHeight);
-      setHeaderHeight(headerRef.current.offsetHeight);
-    }
-  }, []);
+    useEffect(() => {
+        if (headerRef.current && headerRef.current.offsetHeight) {
+            console.log('Height of header:', headerRef.current.offsetHeight);
+            setHeaderHeight(headerRef.current.offsetHeight);
+        }
+    }, []);
 
-  const handleToggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen); // Змінюємо стан на протилежний
-  };
+    const handleToggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen); // Змінюємо стан на протилежний
+    };
 
-  const handleCloseMenu = () => {
-    setIsMenuOpen(false); // Закриваємо меню
-  };
+    const handleCloseMenu = () => {
+        setIsMenuOpen(false); // Закриваємо меню
+    };
 
-  return (
-    <div className="stickyHeaderContainer" ref={headerRef}>
-      <Sticky stickyClassName={'header_sticky_state'} boundaryElement=".block" hideOnBoundaryHit={false}>
-        <Navbar expand="lg" className={`header_navbar`}>
-          <Container fluid={"lg"} className={"d-flex flex-wrap"}>
-            <Navbar.Brand className={"navbar-logo text-white"} href="#home">TIC.TEREBOVLIA</Navbar.Brand>
-            <Navbar.Toggle
-              className={'border-0 p-0 rounded-0 shadow-none outline-0 text-white'}
-              aria-controls="offcanvasNavbar"
-              onClick={handleToggleMenu} // Викликаємо функцію відкривання/закривання меню
-            >
-              <i className="bi bi-list fs-1 text-white"></i>
-            </Navbar.Toggle>
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar`}
-              aria-labelledby={`offcanvasNavbar`}
-              placement="end"
-              show={isMenuOpen} // Контролюємо відкриття меню через стан
-              onHide={handleCloseMenu} // Функція для закриття меню
-            >
-              <Offcanvas.Header closeButton closeVariant={'white'}>
-                <Offcanvas.Title id={`offcanvasNavbar`}>
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body className={"ps-4 pe-4 pe-lg-0 ps-lg-0"}>
-                <Nav className="justify-content-end flex-grow-1 me-auto header_nav_menu align-items-lg-center">
-                  <Nav.Link as={Link} to="about" smooth={true} duration={500} onClick={handleCloseMenu}>ПРО НАС
-                    <span className={"hover_bl"}></span>
-                  </Nav.Link>
-                  <Nav.Link as={Link} to="services" smooth={true} duration={500} onClick={handleCloseMenu}>ПОСЛУГИ
-                    <span className={"hover_bl"}></span>
-                  </Nav.Link>
-                  <Button to="contacts" smooth={true} duration={500}
-                          className={" btn border-2 rounded-0 btn-lg btn-outline-secondary text-white"}
-                          onClick={handleCloseMenu}>КОНТАКТИ</Button>
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      </Sticky>
-    </div>
-  );
+    return (
+        <div className="stickyHeaderContainer" ref={headerRef}>
+            <Sticky stickyClassName={'header_sticky_state'} boundaryElement=".block" hideOnBoundaryHit={false}>
+                <Navbar expand="lg" className={`header_navbar`}>
+                    <Container fluid={"lg"} className={"d-flex flex-wrap"}>
+                        <Navbar.Brand className={"navbar-logo text-white"} href="#home">TIC.TEREBOVLIA</Navbar.Brand>
+                        <Navbar.Toggle
+                            className={'border-0 p-0 rounded-0 shadow-none outline-0 text-white'}
+                            aria-controls="offcanvasNavbar"
+                            onClick={handleToggleMenu} // Викликаємо функцію відкривання/закривання меню
+                        >
+                            <i className="bi bi-list fs-1 text-white"></i>
+                        </Navbar.Toggle>
+                        <Navbar.Offcanvas
+                            id={`offcanvasNavbar`}
+                            aria-labelledby={`offcanvasNavbar`}
+                            placement="end"
+                            show={isMenuOpen} // Контролюємо відкриття меню через стан
+                            onHide={handleCloseMenu} // Функція для закриття меню
+                        >
+                            <Offcanvas.Header closeButton closeVariant={'white'}>
+                                <Offcanvas.Title id={`offcanvasNavbar`}>
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body className={"ps-4 pe-4 pe-lg-0 ps-lg-0"}>
+                                <Nav
+                                    className="justify-content-end flex-grow-1 me-auto header_nav_menu align-items-lg-center">
+                                    <Nav.Link as={Link} to="about" smooth={true} duration={500}
+                                              onClick={handleCloseMenu}>ПРО НАС
+                                        <span className={"hover_bl"}></span>
+                                    </Nav.Link>
+                                    <Nav.Link as={Link} to="services" smooth={true} duration={500}
+                                              onClick={handleCloseMenu}>ПОСЛУГИ
+                                        <span className={"hover_bl"}></span>
+                                    </Nav.Link>
+                                    <Button to="contacts" smooth={true} duration={500}
+                                            className={" btn border-2 rounded-0 btn-lg btn-outline-secondary text-white"}
+                                            onClick={handleCloseMenu}>КОНТАКТИ</Button>
+                                </Nav>
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Container>
+                </Navbar>
+            </Sticky>
+        </div>
+    );
 };
 
 export default Header;

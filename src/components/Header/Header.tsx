310807c410bbecc2aacab3ac0574a37665052cf1
Aleksandr <aleksdark13@gmail.com>
2024-09-14 12:08:56 +0300

    link functional

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,9 +4,10 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import '../../App.scss';
 import Sticky from 'react-sticky-el';
-import {Link, Button} from 'react-scroll';
+import {scroller} from 'react-scroll';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import {useLocation} from 'react-router-dom';
+import {useLocation, Link as RouterLink, redirect, useNavigate} from 'react-router-dom';
+import {Button} from "react-bootstrap";
 
 interface IHeader {
     setHeaderHeight: (arg0: number) => void;
@@ -16,8 +17,10 @@ const Header = ({setHeaderHeight}: IHeader) => {
     const headerRef = useRef<HTMLDivElement>(null);
     const [isMenuOpen, setIsMenuOpen] = useState(false); // Стан для відкритого меню
     const [isSticky, setIsSticky] = useState(true); // Стан для застійного підвищення заголовка]
-
     const location = useLocation();
+    const navigation = useNavigate()
+    const [targetId, setTargetId] = useState(null); // зберігаємо ID блоку для скролу
+
 
     // Перевірка поточного шляху
     const isMainPage = location.pathname === '/';
@@ -49,12 +52,40 @@ const Header = ({setHeaderHeight}: IHeader) => {
         setIsMenuOpen(false); // Закриваємо меню
     };
 
+    const handleClick = (id) => {
+        // Встановлюємо цільовий блок
+        setTargetId(id);
+        // Переходимо на головну сторінку
+        if (!isMainPage) {
+            navigation('/');
+        }
+        handleCloseMenu();
+    };
+
+    // Використовуємо useEffect для перевірки, коли перехід вже відбувся
+    useEffect(() => {
+        if (location.pathname === '/' && targetId) {
+            const element = document.getElementById(targetId);
+            if (element) {
+                scroller.scrollTo(targetId, {
+                    behavior: "smooth",
+                    block: "start"
+                })
+            }
+            // Очищуємо targetId після скролу
+            setTargetId(null);
+        }
+    }, [location, targetId]);
+
     return (
         <div className={`stickyHeaderContainer ${!isSticky ? "header_static_state" : ""}`} ref={headerRef}>
-            <Sticky disabled={!isSticky} stickyClassName={'header_sticky_state'} boundaryElement=".block" hideOnBoundaryHit={false}>
+            <Sticky disabled={!isSticky} stickyClassName={'header_sticky_state'} boundaryElement=".block"
+                    hideOnBoundaryHit={false}>
                 <Navbar expand="lg" className={`header_navbar`}>
                     <Container fluid={"xl"} className={"d-flex flex-wrap"}>
-                        <Navbar.Brand className={"navbar-logo text-white"} href="#home">TIC.TEREBOVLIA</Navbar.Brand>
+                        <Navbar.Brand className={"navbar-logo text-white"}>
+                            <RouterLink to={'/'}>TIC.TEREBOVLIA</RouterLink>
+                        </Navbar.Brand>
                         <Navbar.Toggle
                             className={'border-0 p-0 rounded-0 shadow-none outline-0 text-white'}
                             aria-controls="offcanvasNavbar"
@@ -76,17 +107,20 @@ const Header = ({setHeaderHeight}: IHeader) => {
                             <Offcanvas.Body className={"ps-4 pe-4 pe-lg-0 ps-lg-0"}>
                                 <Nav
                                     className="justify-content-end flex-grow-1 me-auto header_nav_menu align-items-lg-center">
-                                    <Nav.Link as={Link} to="about" smooth={true} duration={500}
-                                              onClick={handleCloseMenu}>ПРО НАС
+                                    <Nav.Link
+                                        onClick={() => handleClick('about')}>ПРО НАС
                                         <span className={"hover_bl"}></span>
                                     </Nav.Link>
-                                    <Nav.Link as={Link} to="services" smooth={true} duration={500}
-                                              onClick={handleCloseMenu}>ПОСЛУГИ
+
+                                    <Nav.Link
+                                        onClick={() => handleClick('services')}>ПОСЛУГИ
                                         <span className={"hover_bl"}></span>
                                     </Nav.Link>
-                                    <Button to="contacts" smooth={true} duration={500}
-                                            className={" btn btn-lg btn-outline-secondary text-white"}
-                                            onClick={handleCloseMenu}>КОНТАКТИ</Button>
+
+                                    <Button size={"lg"} variant={"outline-secondary"} to="contacts"
+                                            className={" text-white"}
+                                            onClick={() => handleClick('contacts')}>КОНТАКТИ</Button>
+
                                 </Nav>
                             </Offcanvas.Body>
                         </Navbar.Offcanvas>
